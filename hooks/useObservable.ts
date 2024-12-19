3c2e29ef1d440af9a4dd676863336d319c56287c
Tyler Williams <tyler@coolsoftware.dev>
2024-12-19 12:00:40 -0700

    fix: types and debug utility

--- hooks/useObservable.ts
+++ hooks/useObservable.ts
@@ -1,41 +1,60 @@
 import { reaction } from "mobx";
-import { IStateTreeNode, getMembers, isStateTreeNode } from "mobx-state-tree";
+import {
+  IAnyStateTreeNode,
+  IStateTreeNode,
+  getMembers,
+  isStateTreeNode,
+} from "mobx-state-tree";
 import { useCallback, useLayoutEffect, useRef, useState } from "react";
 
 const emptyObject = {};
 
-export function useObservable<T extends IStateTreeNode>(model: T) {
+type ViewArgs<T> = T extends (...args: infer A) => unknown ? A : never;
+
+class Path<T extends IAnyStateTreeNode> {
+  parts: (string | symbol)[];
+  args?: ViewArgs<T>;
+
+  constructor(parts: (string | symbol)[], args?: ViewArgs<T>) {
+    this.parts = parts;
+    this.args = args;
+  }
+
+  toString() {
+    return this.parts.join(".");
+  }
+}
+
+export function useObservable<T extends IStateTreeNode>(
+  model: T,
+  debug?: boolean
+): T {
   const [, setState] = useState(1);
   const forceRender = useCallback(() => setState((n) => -n), []);
 
-  const pathsAccessedInRender = useRef<Set<string[]>>(new Set());
+  const pathsAccessedInRender = useRef<Set<Path<T>>>(new Set());
   pathsAccessedInRender.current = new Set();
 
   useLayoutEffect(() => {
     const disposer = reaction(
       function expression() {
-        //// Uncomment this to see what paths are being accessed in the render
+        if (debug) {
+          console.log("Paths accessed in render:");
+          pathsAccessedInRender.current.forEach((path) => {
+            console.log("  ", path.toString());
+            if (path.args) {
+              console.log("    This is a view fn, with args:", path.args);
+            }
+          });
+        }
 
-        // console.log("Paths accessed in render:");
-        // pathsAccessedInRender.current.forEach((pathParts) => {
-        //   console.log("  ", pathParts.join("."));
-        //   if (Array.isArray(pathParts.args)) {
-        //     console.log("    This is a view fn, with args:", pathParts.args);
-        //   }
-        // });
+        for (const path of pathsAccessedInRender.current) {
+          let current: IAnyStateTreeNode = model;
+          for (const part of path.parts) {
+            current = current[part as keyof typeof current];
 
-        for (const pathParts of pathsAccessedInRender.current) {
-          let current = model;
-          for (const part of pathParts) {
-            current = current[part];
-
-            // If we are accessing a view function, we attach the args
-            // to the path array so we can call the view function here
-            if (
-              typeof current === "function" &&
-              Array.isArray(pathParts.args)
-            ) {
-              current = current(...pathParts.args);
+            if (typeof current === "function" && path.args) {
+              current = current(...path.args);
             }
           }
         }
@@ -53,14 +72,14 @@ export function useObservable<T extends IStateTreeNode>(model: T) {
     return () => {
       disposer();
     };
-  }, [forceRender, model]);
+  }, [forceRender, model, debug]);
 
-  function makeProxy(instance, path) {
+  function makeProxy(instance: IAnyStateTreeNode, path: (string | symbol)[]) {
     return new Proxy(emptyObject, {
       get(_, key) {
-        const value = instance[key];
+        const value = instance[key as keyof typeof instance];
 
-        const newPath = [...path, key];
+        const newPath = new Path([...path, key as string]);
         pathsAccessedInRender.current.add(newPath);
 
         if (isStateTreeNode(value)) {
@@ -69,15 +88,10 @@ export function useObservable<T extends IStateTreeNode>(model: T) {
 
         if (typeof value === "function") {
           const members = getMembers(instance);
-          if (members.views.includes(key)) {
-            // If we are accessing a view function, we attach the args
-            // to the path array so we can call the view function in the
-            // reaction.
-
+          if (members.views.includes(key as string)) {
             const view = value.bind(instance);
-            const argumentWatcher = (...args: any) => {
-              const newPath = [...path, key];
-              newPath.args = args;
+            const argumentWatcher = (...args: ViewArgs<typeof view>) => {
+              const newPath = new Path([...path, key as string], args);
               pathsAccessedInRender.current.add(newPath);
               return view(...args);
             };
@@ -93,5 +107,5 @@ export function useObservable<T extends IStateTreeNode>(model: T) {
     });
   }
 
-  return makeProxy(model, []);
+  return makeProxy(model, []) as T;
 }
