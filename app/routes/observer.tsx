104ada609f83fb8b06deea070eec36bce456c12c
Tyler Williams <tyler@coolsoftware.dev>
2024-12-18 21:45:11 -0700

    feat: add lowercase function to example pages

--- app/routes/observer.tsx
+++ app/routes/observer.tsx
@@ -12,7 +12,7 @@ export const meta: MetaFunction = () => {
 export default observer(function ObserverPage() {
   const {
     count,
-    submodel: { title, allCaps },
+    submodel: { title, allCaps, lowercase },
   } = store;
 
   console.log("Rendering")
@@ -25,6 +25,7 @@ export default observer(function ObserverPage() {
         {count}: {title}
       </p>
       <p className="text-gray-800 my-2">This is a computed view that capitalizes the title: {allCaps}</p>
+      <p className="text-gray-800 my-2">This is a lazily evaluated view that converts the title to lowercase: {lowercase()}</p>
       <div className="flex flex-wrap gap-2">
         <button 
           onClick={() => store.increment()} 
